cc60a13529466af3e4cae836d5eebe7bc49d18fb
Adrian Czesnowski <adrian.czesnowski22@gmail.com>
2024-01-27 14:50:32 +0100

    NestJS basics: post, put, delete, etc.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  fruits = [{ name: 'Apple' }, { name: 'Banana' }];
   @Get()
   getHello(): string {
     return 'Hello World!';
   }
 
-  @Get('/user')
-  getSample() {
-    return {
-      name: 'Adrian',
-    };
+  @Get('/fruits')
+  getFruit() {
+    return this.fruits;
+  }
+
+  @Post('')
+  createFruit(@Body() fruit: { name: string }) {
+    this.fruits.push(fruit);
+    return { message: 'Fruit created.' };
+  }
+
+  @Put(':fruitId')
+  updateFruit(
+    @Body() fruit: { name: string },
+    @Param('fruitId') fruitId: string,
+  ) {
+    return { message: `Fruit ${fruitId} updated.` };
+  }
+
+  @Delete(':fruitId')
+  deleteFruit(@Param('fruitId') fruitId: string) {
+    return { message: `Fruit ${fruitId} deleted.` };
   }
 }

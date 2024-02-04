a2ffdc3fc0d9b75a33e780847b8e9f59b194e6ae
Adrian Czesnowski <adrian.czesnowski22@gmail.com>
2024-02-04 14:17:59 +0100

    adding typeorm and sqlite3

--- src/recipe/recipe.module.ts
+++ src/recipe/recipe.module.ts
@@ -3,8 +3,12 @@ import { DishesController } from './dishes/dishes.controller';
 import { ProductsController } from './products/products.controller';
 import { DishesService } from './dishes/dishes.service';
 import { ProductsService } from './products/products.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './products/Products';
+import { Dish } from './dishes/Dish';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product, Dish])],
   controllers: [DishesController, ProductsController],
   providers: [DishesService, ProductsService],
 })
